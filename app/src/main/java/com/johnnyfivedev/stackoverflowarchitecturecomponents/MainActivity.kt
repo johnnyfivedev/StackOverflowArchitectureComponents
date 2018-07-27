69ee0cc4d16a06a0e22010203170ad96d9dd4c9f
Johnny Five <19605186+johnnyfivedev@users.noreply.github.com>
2018-07-27 17:30:09 +0300

    Add retrofit

--- app/src/main/java/com/johnnyfivedev/stackoverflowarchitecturecomponents/MainActivity.kt
+++ app/src/main/java/com/johnnyfivedev/stackoverflowarchitecturecomponents/MainActivity.kt
@@ -2,11 +2,16 @@ package com.johnnyfivedev.stackoverflowarchitecturecomponents
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.tv_text as tvText
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
+
