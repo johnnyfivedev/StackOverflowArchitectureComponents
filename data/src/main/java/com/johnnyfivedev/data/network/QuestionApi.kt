9be3192652dd4c550d51d0ec62d28ad5c9eae854
Johnny Five <19605186+johnnyfivedev@users.noreply.github.com>
2018-08-07 16:57:57 +0300

    Limit question request page size

--- data/src/main/java/com/johnnyfivedev/data/network/QuestionApi.kt
+++ data/src/main/java/com/johnnyfivedev/data/network/QuestionApi.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 
 interface QuestionApi {
 
-    @GET("/2.2/questions?order=desc&sort=activity&site=stackoverflow")
+    @GET("/2.2/questions?pagesize=10&order=desc&sort=activity&site=stackoverflow&limit=10")
     fun getQuestions(): Flowable<QuestionsResponse>
 }
